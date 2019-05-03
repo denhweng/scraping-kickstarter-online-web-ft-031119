e681de48fb4ab111a1f9a9d3db9bd62053cfa95c
Dan Hwang <danhwangfkpc@gmail.com>
2019-05-03 08:33:01 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -19,11 +19,3 @@ def create_project_hash
   
   projects
 end
-
-
-
-
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-# location: project.css("u1.project-meta span.location-name").text 
-# percent_funded: project.css("u1.project-stats li.first.funded strong").text.gsub("%","").to_i 
